b94a2d054073517df140c8912275bb53675abd6c
Ivan Omelchenko <oduvanchik22@gmail.com>
2018-09-06 16:13:41 +0300

    Add project files.

---
+++ WebApiProject/Models/TextSearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProject.Models
+{
+	public class TextSearchModel
+	{
+		public string TextForSearch { get; set; }
+	}
+}
