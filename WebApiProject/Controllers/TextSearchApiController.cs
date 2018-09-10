4a6209cb509f9bdbc70c2b2692600b2a28ffa092
Ivan Omelchenko <oduvanchik22@gmail.com>
2018-09-10 18:21:16 +0300

    added queue to web api service

--- WebApiProject/Controllers/TextSearchApiController.cs
+++ WebApiProject/Controllers/TextSearchApiController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using RabbitMQ.Client;
 using WebApiProject.Models;
 
 namespace WebApiProject.Controllers
@@ -11,10 +13,34 @@ namespace WebApiProject.Controllers
     [ApiController]
     public class TextSearchApiController : ControllerBase
     {
+		private readonly ConnectionFactory _connectionFactory;
+
+		public TextSearchApiController()
+		{
+			_connectionFactory = new ConnectionFactory { HostName = "localhost" };
+		}
+
 		[HttpPost("/document")]
 		public async Task<ActionResult> UploadDocument(IFormFile file)
 		{
-			return Ok();
+			if (file.Length > 0)
+			{
+				using (MemoryStream stream = new MemoryStream())
+				{
+					await file.CopyToAsync(stream, new System.Threading.CancellationToken());
+
+					using (var connection = _connectionFactory.CreateConnection())
+					using (var channel = connection.CreateModel())
+					{
+						channel.QueueDeclare("documentQueue", false, false, false);
+
+						channel.BasicPublish("", "documentQueue", null, stream.GetBuffer());
+					}
+					return Ok();
+				}
+			}
+
+			return BadRequest();
 		}
 
 		[HttpGet("/documents")]
